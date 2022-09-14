43dbfb65d68584e4917a817856062c5ae610bbc7
Lívio Lopes <antonio.lvio760@gmail.com>
2022-09-14 17:00:23 -0300

    item 3 resolvido

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputEmail = document.querySelector('#input-email')
+const inputPassword = document.querySelector('#input-password');
+const inputButton = document.querySelector('#button-login');
+
+Element.prototype.on = Element.prototype.addEventListener;
+
+inputButton.on('click',function(event){
+    event.preventDefault();
+    let email = inputEmail.value;
+    let pwd = inputPassword.value;
+    if(email === 'tryber@teste.com' && pwd === '123456'){
+        alert('Olá, Tryber!');
+    }else{
+        alert('Email ou senha inválidos.');
+    }
+});
